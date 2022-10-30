85321c9c506264180f3475bea39e4b8f4cd87190
chienkt <kennyjoe1311@gmail.com>
2022-10-30 22:39:44 +0700

    remove redudant import

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,14 +1,13 @@
-import react from 'react';
 import Footer from '~/components/Layout/footer/Footer';
 import Header from '~/components/Layout/header/Header';
 
-function Cart (){
-    return (
-        <div>
-            <Header />
-            <Footer/>
-        </div>
-    );
+function Cart() {
+   return (
+      <div>
+         <Header />
+         <Footer />
+      </div>
+   );
 }
 
 export default Cart;
